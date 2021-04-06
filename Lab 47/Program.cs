d8b1bbd013a0e38eb7f8386c3565579ceabf3f56
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-06 23:06:36 +0300

    Array manipulation

---
+++ Lab 47/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
+            Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums));
+            var sum = 0;
+            foreach(int num in nums)
+            {
+                sum+=num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
