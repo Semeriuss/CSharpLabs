82b3d127d657a5346fc3f5fc6d9efdf10f85c8c3
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-06 22:10:51 +0300

    conditionals

---
+++ Lab 37/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Test Data: ");
+            string str = Console.ReadLine();
+            Console.WriteLine((str.Substring(1, 2).Equals("HP") ? str.Remove(1, 2) : str));
+        }
+    }
+}
---
+++ Lab 38/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Test Data: ");
+            string str = Console.ReadLine();
+            var result = "";
+
+            if (str.Length >= 1 && str[0] == 'P') 
+                result += str[0];
+            if (str.Length >= 2 && str[1] == 'H')
+                result += str[1];
+            Console.WriteLine(result);
+        }
+    }
+}
