4bdc9f018be9447e27a736d78c7f28b4f9840179
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-06 23:01:23 +0300

    Array manipulation

---
+++ Lab 45/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nInput an integer:");  
+            int x = Convert.ToInt32(Console.ReadLine());
+            int[] nums = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 9};
+            Console.Write("Number of " + x + " present in the said array: ");
+            Console.WriteLine(nums.Count(n => n == x));
+        }
+    }
+}
