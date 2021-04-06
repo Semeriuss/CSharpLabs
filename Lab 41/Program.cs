260c03e02a662c96bb50d27e32062c0b54d7a1e9
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-06 22:47:36 +0300

    string manipulation questions

---
+++ Lab 41/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a string (conatins at least one 'w' char) : ");
+            string str = Console.ReadLine();
+            var count = str.Count(s => s == 'w');
+            Console.WriteLine("Test the string contains 'w' character  between 1 and 3 times: ");
+            Console.WriteLine(count>=1 && count <= 3);  
+        }
+    }
+}
---
+++ Lab 43/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+            Console.Write("Input a string : ");
+            string str = Console.ReadLine();
+            Console.WriteLine(test(str));
+    }
+    public static bool test(string str)
+       {
+            var ctr = 0;
+            for (var i = 0; i < str.Length-1; i++)
+            {
+                if (str[i].Equals('w')) ctr++;
+                if(str.Substring(i, 2).Equals("ww") && ctr > 2) 
+                return true;
+            }
+            return false;
+        }
+    }
+}
