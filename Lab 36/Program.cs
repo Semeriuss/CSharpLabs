b2c132cef0471ad2b5c5ef23c8e88ab5cf3d8f37
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-06 21:16:00 +0300

    absolute value, conditional

---
+++ Lab 36/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if(Math.Abs(num1) <= 10 && Math.Abs(num2) <= 10)
+            {
+                Console.WriteLine("True");
+            }else{
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
