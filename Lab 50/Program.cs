9e59c38af8cd6ebaf0a4e0693ca9aaa87363e1cb
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-07 06:23:49 +0300

    Advanced array manipulation

---
+++ Lab 50/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 2, 8};
+            Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums));
+            var temp = nums[0];
+                for (var i = 0; i < nums.Length - 1; i++)
+                {
+                    nums[i] = nums[i + 1];
+                }
+                nums[nums.Length - 1] = temp;
+                Console.WriteLine("\nAfter rotating array becomes: [{0}]", string.Join(", ", nums));
+        }
+    }
+}
