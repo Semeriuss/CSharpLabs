b73ebc0db50659b732163d152da4b6b58c89fb7b
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-14 13:27:34 +0300

    Quadratic Equation for a given range

---
+++ Lab 6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Display values of a function");
+            
+            double x;
+
+            for(int y = -5; y < 6; y++)
+            {
+                Console.WriteLine($"{Math.Pow(y, 2) + 2*y + 1}");
+            }
+        }
+    }
+}
