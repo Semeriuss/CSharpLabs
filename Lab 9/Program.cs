f5ad5e9d10e6fba1c6445a7327f6f24f4691b2ff
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-14 20:04:57 +0300

    Checking data type

---
+++ Lab 9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check type of input");
+
+            Console.Write("Enter string: ");
+            char chr = char.Parse(Console.ReadLine());
+
+            if ((chr == 'a') || (chr == 'e') || (chr == 'i') || (chr == 'o') || (chr == 'u'))
+            {
+                Console.WriteLine("It is a lowercase vowel");
+            }
+            else if ((chr >= '0') && (chr <= '9'))
+            {
+                Console.WriteLine("It is a digit");
+            }
+            else
+            {
+                Console.WriteLine("It is a symbol");
+            }
+        }
+    }
+}
