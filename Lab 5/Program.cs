ba4f4af54381518f73ea1624045e9ca4f59035d3
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-14 13:22:12 +0300

    Circle area|perimeter - string formatter

---
+++ Lab 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate the perimeter and area of a circle");
+
+            Console.Write("Enter radius: ");
+            double rad = double.Parse(Console.ReadLine());
+
+            double perimeter = 2*rad*Math.PI;
+            double area = Math.PI * Math.Pow(rad, 2);
+
+            string formatted_perimeter = String.Format("{0:0.##}", perimeter);
+            string formatted_area = String.Format("{0:0.##}", area);
+
+            Console.WriteLine($"The Perimeter is {formatted_perimeter}\nThe Area is {formatted_area}");
+            
+        }
+    }
+}
