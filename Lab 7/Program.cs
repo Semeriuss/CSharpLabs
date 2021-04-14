2e12cb8d031c554d869190ba1c67c57c52419787
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-14 13:32:53 +0300

    Finding speed given distance and time

---
+++ Lab 7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finding Speed");
+
+            Console.Write("Input distance (m): ");
+            double distance = double.Parse(Console.ReadLine());
+
+            Console.Write("Input time (s): ");
+            double time = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The Speed is: {distance/time} m/s");
+            
+        }
+    }
+}
